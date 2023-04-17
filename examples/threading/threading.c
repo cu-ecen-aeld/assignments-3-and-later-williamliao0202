9a270d7909bbdb8e0a60f8cf1d44cf6ab4a3e6c1
will22 <will22@Ubuntu.myguest.virtualbox.org>
2023-04-17 17:40:54 +0800

    check memory after allocate.

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
@@ -56,7 +57,12 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      */
     //allocate memery for thead_data
     struct thread_data *data = (struct thread_data *) malloc(sizeof(struct thread_data));
-
+    if (data == NULL)
+    {
+        ERROR_LOG("Allocate memory for thread_data failed.");
+        return false;
+    }
+    memset(data, 0, sizeof(struct thread_data));
     data->wait_to_obtain_ms = wait_to_obtain_ms;
     data->wait_to_release_ms = wait_to_release_ms;
     data->mutex = mutex;

99c34702d257b4172f4219fe8478d89f001a5005
will22 <will22@Ubuntu.myguest.virtualbox.org>
2023-04-17 17:16:08 +0800

    assignment-4-part-1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -14,6 +14,32 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data *thread_func_args = (struct thread_data *) thread_param;
+    //wait
+    usleep(thread_func_args->wait_to_obtain_ms*1000);
+    //obtain mutex
+    int rc = pthread_mutex_lock(thread_func_args->mutex);
+    if (rc != 0)
+    {
+        ERROR_LOG("Can not lock. Error Code: %d", rc);
+        thread_func_args->thread_complete_success = false;
+    }
+    else
+    {
+        //wait
+        usleep(thread_func_args->wait_to_release_ms*1000);
+        //release mutex
+        rc = pthread_mutex_unlock(thread_func_args->mutex);
+        if (rc != 0)
+        {
+            ERROR_LOG("Can not release. Error Code: %d", rc);
+            thread_func_args->thread_complete_success = false;
+        }
+        else
+        {
+            thread_func_args->thread_complete_success = true;
+        }
+    }
     return thread_param;
 }
 
@@ -28,6 +54,19 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
-}
+    //allocate memery for thead_data
+    struct thread_data *data = (struct thread_data *) malloc(sizeof(struct thread_data));
 
+    data->wait_to_obtain_ms = wait_to_obtain_ms;
+    data->wait_to_release_ms = wait_to_release_ms;
+    data->mutex = mutex;
+    data->thread_complete_success = false;
+
+    int rc = pthread_create(thread, NULL, threadfunc, data);
+    if (rc != 0)
+    {
+        ERROR_LOG("pthread_create failed, Error Code: %d", rc);
+        return false;
+    }
+    return true;
+}
